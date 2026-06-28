b3e42b077b04f4afd6675b966c97cb027b824c10
Hansana Gayathmika <hansanagayathmika2003@gmail.com>
2026-06-28 10:34:50 +0530

    Add book title and price extraction to the web scraper

--- books_to_scrape.py
+++ books_to_scrape.py
@@ -7,4 +7,9 @@ page = requests.get(url).text
 doc = BeautifulSoup(page, 'html.parser')
 
 books = doc.find_all(class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
-print(books)
+
+for book in books:
+    title = book.find('h3').text
+    price = book.find(class_="price_color").text
+    print(title)
+    print(price)
