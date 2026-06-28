f92ae97fc1a78d55bd5c132491d5fd8a7511e53f
Hansana Gayathmika <hansanagayathmika2003@gmail.com>
2026-06-28 10:57:14 +0530

    Add rating extraction to book scraper

--- books_to_scrape.py
+++ books_to_scrape.py
@@ -11,5 +11,11 @@ books = doc.find_all(class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
 for book in books:
     title = book.find('h3').text
     price = book.find(class_="price_color").text
+
+    rating_tag = book.find("p", class_="star-rating")
+    rating = rating_tag['class'][1]
+
     print(title)
     print(price)
+    print(f"rating: {rating}")
+    print("-" * 20)
