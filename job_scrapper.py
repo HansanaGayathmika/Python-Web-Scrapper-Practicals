4b1eb72acfea56e5ce79d7118544f2b86595e2a2
Hansana Gayathmika <hansanagayathmika2003@gmail.com>
2026-06-28 22:18:25 +0530

    Enhance job scraper to filter for Python jobs and extract company names and salaries

--- job_scrapper.py
+++ job_scrapper.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 import time
+import re
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("https://remoteok.com/")
@@ -21,7 +22,23 @@ doc = BeautifulSoup(driver.page_source, 'html.parser')
 job_titles = doc.find_all("tr", attrs={"data-offset": True})
 
 for job in job_titles:
+
+    tags = job.find_all(class_="tag")
+    tag_names = [tag.text.strip().lower() for tag in tags]
+
+    if not re.search(r"python", tag_names, re.IGNORECASE):
+        continue
     title = job.find('h2').text.strip()
+    company = job.find('h3').text.strip()
+    salary_tag = job.find(class_="salary")
+    if salary_tag:
+        salary = salary_tag.text.strip()
+    else:
+        salary = "Salary not available"
+
     print(title)
+    print(company)
+    print(salary)
+    print("-"*20)
 
 driver.quit()
