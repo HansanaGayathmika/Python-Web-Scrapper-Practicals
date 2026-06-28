9a69b71a2753a2bf5b1ce1965a365b75dda97f02
Hansana Gayathmika <hansanagayathmika2003@gmail.com>
2026-06-28 11:29:31 +0530

    Refactor book scraping logic to handle pagination and save data to CSV
    - Updated URL handling to support pagination through the website.
    - Added functionality to write book titles, prices, and ratings to a CSV file.
    - Removed unnecessary print statements for cleaner output.

--- books_to_scrape.py
+++ books_to_scrape.py
@@ -1,21 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "http://books.toscrape.com/"
+base_url = "http://books.toscrape.com/"
+url = "/"
 
-page = requests.get(url).text
-doc = BeautifulSoup(page, 'html.parser')
+with open(r"C:\\Users\\Hansana\\Desktop\\web_scrapper\\books.csv", "w", encoding="utf-8")as file:
+    while url:
+        page = requests.get(base_url + url).text
+        doc = BeautifulSoup(page, 'html.parser')
+        books = doc.find_all(class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
+        for book in books:
+            title = book.find('h3').text
+            price = book.find(class_="price_color").text
 
-books = doc.find_all(class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
+            rating_tag = book.find("p", class_="star-rating")
+            rating = rating_tag['class'][1]
 
-for book in books:
-    title = book.find('h3').text
-    price = book.find(class_="price_color").text
+            file.write(f"{title}\n")
+            file.write(f"{price}\n")
+            file.write(f"{rating}\n")
+            file.write("\n")
 
-    rating_tag = book.find("p", class_="star-rating")
-    rating = rating_tag['class'][1]
-
-    print(title)
-    print(price)
-    print(f"rating: {rating}")
-    print("-" * 20)
+        page_text = doc.find(class_="next")
+        if page_text:
+            next_href = page_text.find('a')['href']
+            if "catalogue/" in next_href:
+                url = next_href
+            else:
+                url = "catalogue/" + next_href
+        else:
+            url = None
